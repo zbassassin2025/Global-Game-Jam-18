0456d1c8096f65b87df730c99ed7ac34336e4777
Kuan-Ping Chang <masterkuanthun@hotmail.com>
2018-01-27 12:31:03 -0800

    Player Prefab Update and Some UI
    Added Player Stats: hp
    Some UI Function
    Some placeholder
    Add Font

---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+
+	public float hp; //hitpoint 
+
+	void Awake(){
+
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
