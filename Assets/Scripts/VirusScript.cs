aa06fe81fc6ba5fda6c289ebf57bafa8bc6db571
zac bogner <zac.bogner@gmail.com>
2018-01-27 12:01:57 -0800

    Implementing virsu script

--- Assets/Scripts/VirusScript.cs
+++ Assets/Scripts/VirusScript.cs
@@ -15,6 +15,8 @@ public class VirusScript : MonoBehaviour
     public Transform playerPos;
     public GameObject target;
 
+    private bool targetinRange = false;
+
     public float range = 5;
 
     public VirusID vID = VirusID.VirusAttack;
@@ -39,21 +41,43 @@ public class VirusScript : MonoBehaviour
         VirusDead
     }
 
+    private void Update()
+    {
+        StartCoroutine(VirusAttack());
+    }
+
     public IEnumerator VirusAttack()
     {
         yield return new WaitForSeconds(5f);
 
+        float distance = Vector3.Distance(virusPosition.position, playerPos.position);
+
         if (vID == VirusID.VirusAttack)
         {
-            if (Vector3.Distance(virusPosition.position, playerPos.position) <= range)
+           if(distance < 5)
             {
+                targetinRange = true;
+            }
+         
+            if(targetinRange == true)
+            {
+                Debug.Log("enemy is close");
                 virusPosition.LookAt(playerPos);
                 Vector3 moveVirus = virusPosition.position;
-                moveVirus.x = range;
+                //  moveVirus.x = range;
             }
 
-            Vector3.MoveTowards(virusGameObject.transform.position, target.transform.position, range);
+            Vector3 area = virusGameObject.transform.position - target.transform.position.normalized;
+            float moveSpeed = virusSpeed * Time.deltaTime;
+            virusGameObject.transform.position = target.transform.position + (area * moveSpeed);
+            transform.LookAt(target.transform);
         }
+        else
+        {
+            Debug.Log("not close yet " + distance);
+        }
+
+        yield return new WaitForSeconds(1.0f);
     }
 
     public IEnumerator VirusMultiply()
