12835236f7c0685f5eb817ec04b80b11ff81a054
japos003 <jrcapostol@gmail.com>
2018-01-27 11:46:21 -0800

    adding ProjectionScript.cs

---
+++ Assets/Scripts/ProjectionScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectionScript : MonoBehaviour {
+    //test
+
+    public float _movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.up * Time.deltaTime * _movementSpeed);
+	}
+}
