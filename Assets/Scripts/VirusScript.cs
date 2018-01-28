3a08076aed0156b22fc4c63acaa2d447e53e38f9
Kuan-Ping Chang <masterkuanthun@hotmail.com>
2018-01-27 19:47:20 -0800

    Virus Multiple Spawn

--- Assets/Scripts/VirusScript.cs
+++ Assets/Scripts/VirusScript.cs
@@ -49,10 +49,25 @@ public class VirusScript : MonoBehaviour
     void OnCollisionEnter(Collision col) // destroy
     {
         if (col.transform.tag == "Blood")
-        {
-			//anim.Play("BateriophageInfection");
+		{
+			Destroy(col.gameObject);
 			anim.SetTrigger("bacteriophageInfection");
-            Destroy(col.gameObject);
+			//virus_animation();
+			//Destroy(gameObject);
+
         }
     }
+
+	IEnumerator virus_animation(){
+
+		//anim.Play("BateriophageInfection");
+		//float delay = 10f;
+		anim.SetTrigger("bacteriophageInfection");
+		//Debug.Log("time: " + anim.GetCurrentAnimatorStateInfo(0).length + delay); 
+		//Destroy(gameObject, anim.GetCurrentAnimatorStateInfo(0).length + delay); 
+	
+		yield return null;
+	}
+
+
 }
---
+++ Assets/virus_animation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class virus_animation : MonoBehaviour {
+
+
+	public GameObject virus;
+
+	// Use this for initialization
+	void Start () {
+
+		//Changing tag for this specific one
+		virus = GameObject.FindGameObjectWithTag("Virus");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		virus = GameObject.FindGameObjectWithTag("Virus");
+	}
+
+	public void DestroyVirus(){
+
+		Instantiate(virus, transform.position, Quaternion.identity).SetActive(true);
+		Destroy(transform.parent.gameObject);
+	}
+
+
+}
